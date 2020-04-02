a98bc4f1d8202f31f40569ad144683bd6e99ccb6
Daniel Fischer <daniel@d-fischer.dev>
2020-04-02 21:42:12 +0200

    4.0.1

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,12 @@
 /// <reference lib="dom" />
 
-declare const _fetch: typeof fetch;
-declare const _Request: typeof Request;
-declare const _Response: typeof Response;
-declare const _Headers: typeof Headers;
+declare const _fetch: typeof globalThis.fetch;
+declare const _Request: typeof globalThis.Request;
+declare const _Response: typeof globalThis.Response;
+declare const _Headers: typeof globalThis.Headers;
 
-declare module "cross-fetch" {
-  export const fetch: typeof _fetch;
-  export const Request: typeof _Request;
-  export const Response: typeof _Response;
-  export const Headers: typeof _Headers;
-  export default fetch;
-}
+export const fetch: typeof _fetch;
+export const Request: typeof _Request;
+export const Response: typeof _Response;
+export const Headers: typeof _Headers;
+export default fetch;
